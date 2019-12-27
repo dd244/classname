6360daa26d9e9782129c71ac42595887d54e4d22
dd244 <52167916+dd244@users.noreply.github.com>
2019-12-27 23:57:15 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+class Human {
+    name;
+    job;
+    skills;
+    constructor(){
+     this.name="Jim"
+     this.job="Doctor"
+     this.skills="Skating"
+     getJob();{
+    
+     }
+
+     leaveJob();{
+
+     }
+     
+     learnNewSkill();{
+
+     }
+     
+     forgetSkill();{
+
+     }
+     class Student extends Human{
+
+      // Finish this class
+  
+  }
+  
+  
+  const me = new Student();
+  
+  me.getJob ("Web developer");
+  
+  me.learnNewSkill("React");
+  
+  me.forgetSkill("React");
+  
+  me.leaveJob();
+
+  
+}
+}
+
+  );
+  }
+export default App
